2fb274d0791c5a25a2bfd98c5c9a5375578a3390
alexandrosgiohalas <alexandrosgiohalas@gmail.com>
2019-02-21 12:44:43 +0200

    compute mAP of low dimension embedding

---
+++ keyword_spotting/feature_vectors/Learning-Free-KWS-master/sample/performance/retrieve.m
@@ -0,0 +1,29 @@
+descriptors = matfile('iso_desc');
+queries = matfile('SampleRelevantList');
+wmatname = 'words_POG_Sequential';
+wordsmat = matfile(['temp_descriptors/' wmatname]);
+wnames = wordsmat.names;
+N = numel(queries.queriesInfo);
+AP = zeros(1,N);
+Pa5 = zeros(1,N);
+wdesc = descriptors.wDescriptors;
+queryInfo = queries.queriesInfo;
+for i = 1:N
+    name = queryInfo(i).name;
+    name = string(name);
+    idx = find(wnames == name);
+    query = descriptors.wDescriptors(idx,:);
+    Dist = pdist2(query,wdesc);
+    [~,sorted_id] = sort(Dist);
+    relevantList = queryInfo(i).relevantList;
+    swnames = wnames(sorted_id);
+    if (~isempty(queryInfo(i).self))
+        is = ismember(swnames,queriesInfo(ii).self);
+        swnames(is) = [];
+    end
+    ids = ismember(swnames,relevantList);
+    [AP(i),Pa5(i)] = eval_metrics(ids);
+end
+disp('-----Results-----');
+    disp([' P@5 : ', num2str(mean(Pa5))]);
+    disp([' MAP : ', num2str(mean(AP))]);
